979a438324cd07cf8eedc82bf25f5ef2ba374fbf
Pardeep Kumar <pardeep.kumar02@nagarro.com>
2024-06-30 17:27:52 +0530

    Generelized the validation directives through an abstract class AbstractValidatorDirective

--- src/forms/src/directives/validators.ts
+++ src/forms/src/directives/validators.ts
@@ -1,4 +1,4 @@
-import { Directive, InjectionToken, Input } from "@angular/core"
+import { Directive, InjectionToken, Input, OnChanges, SimpleChanges } from "@angular/core"
 import { AbstractControl } from "../model/AbstractControl"
 
 export type ValidationErrors = {
@@ -15,6 +15,59 @@ export interface Validator {
 
 export const NG_VALIDATORS: InjectionToken<Validator> = new InjectionToken("NG_VALIDATOR")
 
+export function nullValidator(control: AbstractControl): null {
+    return null;
+}
+
+export function toBool(input: string|boolean): boolean {
+    return typeof input === "boolean" ? input : input !== null && input !== "false";
+}
+
+export function toInteger(input: string|number): number {
+    return typeof input === "number" ? input : parseInt(input, 10);
+}
+
+export function requiredValidator(input: boolean): ValidatorFn {
+    if (input) {
+        return (control: AbstractControl): ValidationErrors|null => {
+            return control.value ? null : { required: true }
+        }
+    }
+    return nullValidator
+}
+
+export function minlengthValidator(minlength: number): ValidatorFn {
+   return (control: AbstractControl): ValidationErrors|null => {
+    if (control.value.length < minlength) {
+        return {
+          minlength: {
+            actualLength: control.value.length,
+            requiredLength: minlength,
+          },
+        };
+    }
+    return null;
+   }
+}
+
+@Directive()
+export abstract class AbstractValidatorDirective  implements Validator, OnChanges {
+    private _validator: ValidatorFn = nullValidator;
+    abstract inputName: string;
+    abstract createValidator(input: unknown): ValidatorFn;
+    abstract normalizeInput(input: unknown): unknown;
+    validate(control: AbstractControl<any>): ValidationErrors | null {
+        return this._validator(control);
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.inputName in changes) {
+            const input = this.normalizeInput(changes[this.inputName].currentValue);
+            this._validator = this.createValidator(input);
+        }
+    }
+
+}
+
 @Directive({
     selector: ":not([type=checkbox])[required][ngModel]",
     providers: [{
@@ -23,10 +76,11 @@ export const NG_VALIDATORS: InjectionToken<Validator> = new InjectionToken("NG_V
         multi: true
     }]
 })
-export class RequiredValidator implements Validator {
-    validate(control: AbstractControl<any>): ValidationErrors | null {
-        return control.value ? null : { required: true };
-    }
+export class RequiredValidator extends AbstractValidatorDirective {
+    @Input() required!: string|boolean
+    override inputName: string = "required";
+    override createValidator = requiredValidator;
+    override normalizeInput = toBool;
 }
 
 @Directive({
@@ -37,17 +91,9 @@ export class RequiredValidator implements Validator {
         multi: true
     }]
 })
-export class MinLengthValidatorValidator implements Validator {
+export class MinLengthValidatorValidator extends AbstractValidatorDirective {
     @Input() minlength!: string|number;
-    validate(control: AbstractControl<any>): ValidationErrors | null {
-        if (control.value.length < +this.minlength) {
-            return {
-              minlength: {
-                actualLength: control.value.length,
-                requiredLength: this.minlength,
-              },
-            };
-        }
-        return null;
-    }
+    override inputName: string = "minlength";
+    override createValidator = minlengthValidator
+    override normalizeInput = toInteger
 }
